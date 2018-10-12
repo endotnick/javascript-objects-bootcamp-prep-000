e4c0afdafe064d83084ecfb884732d096e424e75
Nick Rodriguez <nickbenrodriguez@gmail.com>
2018-10-12 22:01:58 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,3 +3,7 @@ const playlist = { 'artist': 'song'};
 const updatePlaylist = (playlist, artistName, songTitle) => {
   playlist[artistName] = songTitle;
 }
+
+const removeFromPlaylist = (playlist, artistName, songTitle) => {
+  delete playlist[artistName];
+}
